b4c709c054a364a87169044951201fc1a7905281
Filip Noetzel <filip@j03.de>
2011-12-22 10:51:18 +0100

    make form-in features it's own jasmine spec

--- src/json_schema_component.specs.js
+++ src/json_schema_component.specs.js
@@ -8,66 +8,69 @@ beforeEach(function () {
 
 describe("JsonSchemaComponent", function() {
 
-  it("should throw an error when invoked w/o arguments", function() {
-    try {
-      new JsonSchemaComponent();
-    } catch (error) {
-      expect(error.message).toEqual(
-        "JsonSchemaComponent needs arguments, like this: 'new JsonSchemaComponent({...);'.");
-    }
-  });
+  describe('form-in features', function() {
 
-  it("should throw an error when invoked w/o schema", function() {
-    try {
-      new JsonSchemaComponent({textarea:"bar"});
-    } catch (error) {
-      expect(error.message).toEqual(
-        "JsonSchemaComponent needs a schema, like this: 'new JsonSchemaComponent({schema:{...});'.");
-    }
-  });
+    it("should throw an error when invoked w/o arguments", function() {
+      try {
+        new JsonSchemaComponent();
+      } catch (error) {
+        expect(error.message).toEqual(
+          "JsonSchemaComponent needs arguments, like this: 'new JsonSchemaComponent({...);'.");
+      }
+    });
 
-  it("should throw an error when invoked w/o textarea", function() {
-    try {
-      new JsonSchemaComponent({schema:{}});
-    } catch (error) {
-      expect(error.message).toEqual(
-        "JsonSchemaComponent needs a textarea, like this: 'new JsonSchemaComponent({textarea:'#...');'.");
-    }
-  });
+    it("should throw an error when invoked w/o schema", function() {
+      try {
+        new JsonSchemaComponent({textarea:"bar"});
+      } catch (error) {
+        expect(error.message).toEqual(
+          "JsonSchemaComponent needs a schema, like this: 'new JsonSchemaComponent({schema:{...});'.");
+      }
+    });
 
-  it("should should fill values in a simple provided form with one text field", function() {
-    fixture.html('<textarea id=testtextarea>{"field_a":"value_a"}</textarea>'+
-                 '<form id=testform><input type=text id="testtextfield" name=field_a /></form>');
+    it("should throw an error when invoked w/o textarea", function() {
+      try {
+        new JsonSchemaComponent({schema:{}});
+      } catch (error) {
+        expect(error.message).toEqual(
+          "JsonSchemaComponent needs a textarea, like this: 'new JsonSchemaComponent({textarea:'#...');'.");
+      }
+    });
 
-    new JsonSchemaComponent({schema:{}, textarea:"#testtextarea", form:"#testform"});
+    it("should should fill values in a simple provided form with one text field", function() {
+      fixture.html('<textarea id=testtextarea>{"field_a":"value_a"}</textarea>'+
+                   '<form id=testform><input type=text id="testtextfield" name=field_a /></form>');
 
-    expect($("#testtextfield").val()).toEqual("value_a");
-  });
+      new JsonSchemaComponent({schema:{}, textarea:"#testtextarea", form:"#testform"});
 
-  it("should should fill boolean values in a provided form with two checkboxes", function() {
-    fixture.html('<textarea id=testtextarea>{"field_a": true, "field_b": false}</textarea>'+
-                 '<form id=testform>'+
-                 '<input type=checkbox id="testcheckbox_a" name=field_a />'+
-                 '<input type=checkbox id="testcheckbox_b" name=field_b />'+
-                 '</form>');
+      expect($("#testtextfield").val()).toEqual("value_a");
+    });
 
-    new JsonSchemaComponent({schema:{}, textarea:"#testtextarea", form:"#testform"});
+    it("should should fill boolean values in a provided form with two checkboxes", function() {
+      fixture.html('<textarea id=testtextarea>{"field_a": true, "field_b": false}</textarea>'+
+                   '<form id=testform>'+
+                   '<input type=checkbox id="testcheckbox_a" name=field_a />'+
+                   '<input type=checkbox id="testcheckbox_b" name=field_b />'+
+                   '</form>');
 
-    expect($("#testcheckbox_a").is(":checked")).toBeTruthy();
-    expect($("#testcheckbox_b").is(":checked")).toBeFalsy();
-  });
+      new JsonSchemaComponent({schema:{}, textarea:"#testtextarea", form:"#testform"});
 
-  it("should should fill boolean values in a provided form with two radio boxes", function() {
-    fixture.html('<textarea id=testtextarea>{"field": ["b"]}</textarea>'+
-                 '<form id=testform>'+
-                 '<input type=radio id="testradiobox_a" name=field value="a" />'+
-                 '<input type=radio id="testradiobox_b" name=field value="b" />'+
-                 '</form>');
+      expect($("#testcheckbox_a").is(":checked")).toBeTruthy();
+      expect($("#testcheckbox_b").is(":checked")).toBeFalsy();
+    });
 
-    new JsonSchemaComponent({schema:{}, textarea:"#testtextarea", form:"#testform"});
+    it("should should fill boolean values in a provided form with two radio boxes", function() {
+      fixture.html('<textarea id=testtextarea>{"field": ["b"]}</textarea>'+
+                   '<form id=testform>'+
+                   '<input type=radio id="testradiobox_a" name=field value="a" />'+
+                   '<input type=radio id="testradiobox_b" name=field value="b" />'+
+                   '</form>');
 
-    expect($("#testradiobox_a").is(":checked")).toBeFalsy();
-    expect($("#testradiobox_b").is(":checked")).toBeTruthy();
+      new JsonSchemaComponent({schema:{}, textarea:"#testtextarea", form:"#testform"});
+
+      expect($("#testradiobox_a").is(":checked")).toBeFalsy();
+      expect($("#testradiobox_b").is(":checked")).toBeTruthy();
+    });
   });
 
   describe('form-out features', function() {

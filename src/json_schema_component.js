715338bd26d4ed6db21eb2ef9d2e8ed5fbec2880
Filip Noetzel <filip@j03.de>
2012-02-02 17:17:46 +0100

    Move builtin template to main source file: simpler
    Initial reason to split was file-size, but that
    can be neglected

--- src/json_schema_component.js
+++ src/json_schema_component.js
@@ -1,4 +1,3 @@
-
 function JsonSchemaComponent(options) {
 
   if (options == null) {
@@ -162,4 +161,34 @@ function JsonSchemaComponent(options) {
     form_in(input_type, name, value, selected);
   });
 }
+
+JsonSchemaComponent.prototype.TEMPLATE = [
+  '<div>',
+  '<h2>${name}</h2>',
+  '{{each(name, properties) properties}}<p><label>',
+  '  {{if properties.description}}<b>${properties.description}</b>{{/if}}',
+  '  {{if properties.type === "boolean"}}',
+  '    <input name="${name}" type="checkbox"/>',
+  '  {{else properties.type === "array"}}',
+  '    <select multiple=multiple name="${name}">',
+  '    {{each(index, value) properties.items.enum}}',
+  '      <option value="${value}">${value}</option>',
+  '    {{/each}}',
+  '    </select>',
+  '  {{else}}', // catches properties.type === "string" and unknown cases
+  '    {{if properties.enum == null}}',
+  '      <input name="${name}" type="text"/>',
+  '    {{else}}',
+  '      <select name="${name}">',
+  '     {{each(index, value) properties.enum}}',
+  '       <option value="${value}">${value}</option>',
+  '     {{/each}}',
+  '    </select>',
+  '  {{/if}}',
+  '{{/if}}',
+  '{{if properties.required === true}}<em>(required)</em>{{/if}}',
+  '{{if properties.required === false}}<em>(optional)</em>{{/if}}',
+  '</label></p>{{/each}}',
+  '</div>'
+].join('\n');
 
